41f233f1931b3fc1ba205429c98fd0a9c04abc72
Vincent Le Goff <vincent.legoff.srs@gmail.com>
2019-08-25 12:46:47 +0200

    Fix setup.py

--- setup.py
+++ setup.py
@@ -6,16 +6,6 @@ with open("README.md", "r") as fh:
 setuptools.setup(
     name="bui",
     version="0.0.1",
-    install_requires = [
-        'wxPython == 4.0.6; platform_system=="Windows"',
-    ],
-    python_requires=">=3.6"
-    extras_require={
-        'demo':  [
-            "aiodns==2.0.0", "aiofiles==0.4.0",
-            "aiohttp==3.5.4", "cchardet==2.1.4",
-        ],
-    },
     author="Vincent Le Goff",
     author_email="vincent.legoff.srs@gmail.com",
     description="The Blind User Interface: the interface you can design with your eyes closed.",
@@ -23,6 +13,16 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/vincent-lg/bui/",
     packages=setuptools.find_packages(),
+    python_requires=">=3.6",
+    install_requires = [
+        'wxPython == 4.0.6; platform_system=="Windows"',
+    ],
+    extras_require={
+        'demo':  [
+            "aiodns==2.0.0", "aiofiles==0.4.0",
+            "aiohttp==3.5.4", "cchardet==2.1.4",
+        ],
+    },
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
